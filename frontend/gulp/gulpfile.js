32232ba8b23e08949cc19c81f2f782b472826f56
vcelim <vickensi.dlcp@gmail.com>
2018-08-28 01:42:42 -0500

    adding-gulpfile

---
+++ frontend/gulp/gulpfile.js
@@ -0,0 +1,24 @@
+const gulp 			= require('gulp'),
+	  sass 			= require('gulp-sass'),
+	  autoprefixer 	= require('gulp-autoprefixer'),
+	  browserSync 	= require('browser-sync');
+
+
+gulp.task('sass', function() {
+	 gulp.src('../src/layouts/*.scss')
+	 	 .pipe(sass())
+	 	 .pipe(gulp.dest('../../dist'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('../src/layouts/*.scss',['sass']);
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["../../dist/*.css"], {
+        server: {
+            baseDir: "../../"
+        }
+    });
+});
+

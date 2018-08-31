33be1b6ba287682bf1a76a3dfc1b52c8a4ea6b00
vcelim <vickensi.dlcp@gmail.com>
2018-08-30 22:48:50 -0500

    update-gulpfile

--- frontend/gulp/gulpfile.js
+++ frontend/gulp/gulpfile.js
@@ -1,15 +1,28 @@
-const gulp          = require('gulp'),
-      sass          = require('gulp-sass'),
-      autoprefixer  = require('gulp-autoprefixer'),
-      browserSync   = require('browser-sync');
+const   gulp          = require('gulp'),
+        sass          = require('gulp-sass'),
+        autoprefixer  = require('gulp-autoprefixer'),
+        browserSync   = require('browser-sync');
 
+let path = {
+    sass: ['../assets/styles/intranet/layouts/**/*.scss'],
+    toAllScssPartials: '../assets/styles/intranet/**/*.scss',
+    css: '../../dist/'
+};
 
 gulp.task('sass', function() {
-      gulp.src('../assets/styles/intranet/layouts/home/style.scss')
-          .pipe(sass())
-          .pipe(gulp.dest('../../dist/home'))
+    gulp.src(path.sass)
+        .pipe(sass())
+        .pipe(gulp.dest(path.css))
 });
 
 gulp.task('watch', function() {
-    gulp.watch('../assets/styles/intranet/**/**/*.scss', ['sass'])
+    gulp.watch(path.toAllScssPartials, ['sass'])
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["../../dist/**/*.css"], {
+        server: {
+            baseDir: "../../"
+        }
+    });
 });

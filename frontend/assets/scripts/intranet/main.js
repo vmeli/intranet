ff3b2177b86a60ac280acad6dc726f932e6b2ca7
vickensi <vickensi.delacruz@glr.pe>
2018-09-07 18:48:02 -0500

    adding-script-swiper

--- frontend/assets/scripts/intranet/main.js
+++ frontend/assets/scripts/intranet/main.js
@@ -2,19 +2,34 @@ window.addEventListener('load', function () {
     vanillaCalendar.init({
         disablePastDays: true
     });
-	var sliderGaleria = new Carousel({
-		elem: 'mol-slider_Main',
-		touch: true,
-		autoplay: true,
-		infinite: true,
-		interval: 3000,
-		initial: 0,
-		dots: true,
-		arrows: true,
-		resize: true,
-		arrowPrev: 'js-Carousel_slide-button-prev',
-		arrowNext: 'js-Carousel_slide-button-next'
-	});
+    var swiper = new Swiper('.swiper-container', {
+		effect: 'fade',
+			autoplay: {
+			delay: 3000,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+    });
+
+	//var sliderGaleria = new Carousel({
+	//	elem: 'mol-slider_Main',
+	//	touch: true,
+	//	autoplay: true,
+	//	infinite: true,
+	//	interval: 3000,
+	//	initial: 0,
+	//	dots: true,
+	//	arrows: true,
+	//	resize: true,
+	//	arrowPrev: 'js-Carousel_slide-button-prev',
+	//	arrowNext: 'js-Carousel_slide-button-next'
+	//});
 	//sliderGaleria.init();
 })
 

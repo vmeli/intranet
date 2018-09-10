64d1b93e48594340ab384469697c1962edefaaf4
vickensi <vickensi.delacruz@glr.pe>
2018-09-10 18:30:46 -0500

    adding-script-slider-swiper

--- frontend/assets/scripts/intranet/main.js
+++ frontend/assets/scripts/intranet/main.js
@@ -4,16 +4,16 @@ window.addEventListener('load', function () {
     });
     var swiper = new Swiper('.swiper-container', {
 		effect: 'fade',
+		loop: true,
 			autoplay: {
-			delay: 3000,
+			delay: 2500,
+		},
+		pagination: {
+			el: '.swiper-pagination',
 		},
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
-		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
 		},
     });
 

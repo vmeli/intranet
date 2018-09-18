7f1783a079601579fb307a6b08e6d2f1d67d0b8f
vcelim <vickensi.dlcp@gmail.com>
2018-09-18 00:03:36 -0500

    adding-modal-components

--- frontend/assets/scripts/intranet/main.js
+++ frontend/assets/scripts/intranet/main.js
@@ -1,35 +1,28 @@
-window.addEventListener('load', function () {
-    vanillaCalendar.init({
-        disablePastDays: true
-    });
-    var swiper = new Swiper('.swiper-container', {
-		effect: 'fade',
-		loop: true,
-			autoplay: {
-			delay: 2500,
-		},
-		pagination: {
-			el: '.swiper-pagination',
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-    });
+vanillaCalendar.init({
+    disablePastDays: true
+});
 
-	//var sliderGaleria = new Carousel({
-	//	elem: 'mol-slider_Main',
-	//	touch: true,
-	//	autoplay: true,
-	//	infinite: true,
-	//	interval: 3000,
-	//	initial: 0,
-	//	dots: true,
-	//	arrows: true,
-	//	resize: true,
-	//	arrowPrev: 'js-Carousel_slide-button-prev',
-	//	arrowNext: 'js-Carousel_slide-button-next'
-	//});
-	//sliderGaleria.init();
-})
+let tagBody = document.body,
+	modalContainer = document.getElementById("modal-container"),
+	modalBackground = document.getElementById("modal-background");
+    	
+    console.log(tagBody);
 
+function addingClass() {
+	modalContainer.classList.add("one");
+	tagBody.classList.add("modal-active");
+}
+function closeModal(e) {
+	if ((e.type === 'click' && e.target === modalBackground) || 
+		(e.type === 'keyup' && (e.keycode === 27 || e.which ===27))){
+			modalContainer.classList.add("out");
+			tagBody.classList.remove("modal-active");
+			setTimeout(endAnimation, 3000);
+	}
+}
+function endAnimation() {
+	modalContainer.remove();
+}
+window.addEventListener('load',addingClass);
+modalContainer.addEventListener	('click', closeModal);
+window.addEventListener('keyup', closeModal);
